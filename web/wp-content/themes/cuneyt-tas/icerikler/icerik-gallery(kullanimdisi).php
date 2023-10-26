550905fe0d38b19a9a6e784cc7f43426ca3f41d0
Cüneyt TAŞ <cuneyttas@hotmail.com.tr>
2023-10-26 14:14:15 +0300

    Hatalı tema kaldırıldı.

--- web/wp-content/themes/cuneyt-tas/icerikler/icerik-gallery(kullanimdisi).php
+++
@@ -1,8 +0,0 @@
-<article class="blogGaleri ozet satir">
-
-	<?php // İleride kullanırsam blogGaleri class'ının biçimini değiştiririm. Şu an kullanılmıyor. ?>
-
-	<h2><?php the_title(); ?></h2>
-	<?php the_content(); ?>
-
-</article>
--- web/wp-content/themes/cuneyt-tas/icerikler/icerik-link.php
+++
@@ -1,7 +0,0 @@
-<article class="blogBaglanti ozet satir">
-
-	<?php // İleride kullanırsam blogBaglanti class'ının biçimini değiştiririm. Şu an kullanılmıyor. ?>
-	<a href="<?php echo get_the_content() ?>"><?php the_title(); ?></a>
-	<?php the_content(); ?>
-
-</article>
