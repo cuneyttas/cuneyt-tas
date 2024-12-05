56da4a90c03089746d20c8e300e38b3b2aa4ce4c
Cüneyt TAŞ <cuneyttas@hotmail.com.tr>
2024-12-05 16:32:27 +0300

    Google Site Doğrulama meta etiketi yerleştirildi

--- web/wp-content/themes/cuneyt-tas/header.php
+++ web/wp-content/themes/cuneyt-tas/header.php
@@ -7,6 +7,7 @@
 	 	<meta charset="<?php bloginfo('charset'); ?>">
 	 	<meta name="title" content="<?php bloginfo('name'); ?>">
 	 	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		<meta name="google-site-verification" content="fPPxDiVU4xZOH4IeqvggSgd5itgiO4wobGvwdhItltU" />
 	 	<link rel="shortcut icon" type="image/x-icon" href="<?= get_stylesheet_directory_uri() ?>/images/favicon.ico">
 	 	<?php wp_head(); ?>
 
